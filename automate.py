b355b2d64e691c6573f86fa99deb400c137d113e
foy4748 <faisaljfcl@gmail.com>
2023-06-02 12:25:05 +0600

    Cleaned the HTML file
    Cleared these repeatative sections
        1. Script Tags
        2. Preloader
        3. Header
        4. BreadCrumb
        5. Footer
        6. Scroll to Top button
    
    Converted HTML comments to JSX comments
    Removed excess <body> tag

--- automate.py
+++ automate.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from bs4 import BeautifulSoup as BS4
 
 from rfc import rfc
@@ -13,11 +13,51 @@ htmls = os.listdir(html_folder)
 
 for html in htmls:
     html_path = os.path.join(html_folder,html)
-    html_file = pascalcase(os.path.splitext(os.path.basename(html))[0])
+    component_name = pascalcase(os.path.splitext(os.path.basename(html))[0])
 
     with open(html_path,'r') as file:
         contents = file.read()
         soup = BS4(contents, 'html.parser')
-        #print(soup.body.find_all("script"))
-    print(rfc.format(a=html_file,b=" "))
+        body = soup.body
+
+        # Clearing Script Tags
+        for s in body.select('script'):
+            s.extract()
+
+        # Clearing 
+
+        ## Preloader 
+        body.select('#preloader')[0].extract()       
+
+        ## Header
+        body.select('header')[0].extract()           
+
+        ## BreadCrumb
+        body.select('.breadcrumb-area')[0].extract() 
+
+        ## Footer
+        body.select('.footer-contact-area')[0].extract() 
+        
+        ## Scroll to Top button
+        body.select('#scrollTopButton')[0].extract() 
+
+        ###
+        
+        # Converting Soup ==> PlainText
+        body_txt = str(body)
+
+        # Converting class to className
+        body_txt = re.sub(r'class=\"','className=\"', body_txt)
+
+        # Converting Comments
+        body_txt = re.sub(r"<!--", "{/*", body_txt)
+        body_txt = re.sub(r"-->", "*/}", body_txt)
+
+        # Removing body Tag
+        body_txt = re.sub(r"</?body>","", body_txt)
+        
+        # Creating React File
+        react_file = os.path.join(react_folder, component_name + ".jsx")
+        with open(react_file,'w', encoding='utf-8') as f:
+            f.write(rfc.format(a=component_name,b=body_txt))
 

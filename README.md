b943b1fdb7854301b93f4742ad28a808ca96a478
foy4748 <faisaljfcl@gmail.com>
2023-06-22 12:23:22 +0600

    Updated README.md. Added comments in automate.py

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# HTML to React Converter / Cleaner Python Script
+# HTML to React/Vue Converter / Cleaner Python Script
 
-STAREX Repo: [Github](https://github.com/iideainformatics/stapex)
 
 ## Instructions
 Run this command below once to use this script 
 ``` console
 pip install -r requirements.txt
 ```
+Then tweak the automate.py script to remove certain
+parts of the htmls such as header, footer, scripts, 
+breadcrumb sections, scroll to top, loading animations
+etc. 
+
+You need to look into the html files. Then note down the
+classes and ids of the repeatative parts. Then write them 
+in the automate.py
+
+The rfc.py and sfc.py holds the empty React component and 
+Vue component templates respectively.
+
+
+## WARNING !
+Don't copy the .gitignore file from the Components folder 
+while copying the generated Component files to your project! 
+
+## Finally
+Put your html files in the html folder.
+Make sure your terminal is at the same path as automate.py
+Then, Run this command to execute the script
+
+``` console
+python automate.py
+```
+Then take a look at the Component folder. The generated 
+components will be there.
--- automate.py
+++ automate.py
@@ -1,29 +1,43 @@
 import os, re
 from bs4 import BeautifulSoup as BS4
+from caseconverter import pascalcase
+
+# Import one of these empty template below
 
 # React Empty Component template
 # from rfc import rfc
 
 # Vue Empty Component template
 from sfc import sfc
-from caseconverter import pascalcase
 
+
+# Getting the path of html and Components folder
 BASE_DIR = os.getcwd()
 
 html_folder = os.path.join(BASE_DIR, "html")
 component_folder = os.path.join(BASE_DIR, "Components")
 
+# Getting the path of the html files
 htmls = os.listdir(html_folder)
 
-# Skipping .gitignore
+# Skipping .gitignore in the html folder
+# in the html file list
 htmls.pop(0)
 
+# Iterating through html files
 for html in htmls:
     html_path = os.path.join(html_folder,html)
+
+    # Generataing file name for component in PascalCase
     component_name = pascalcase(os.path.splitext(os.path.basename(html))[0])
 
+    # Reading a single html file
     with open(html_path,'r', encoding='utf-8') as file:
         contents = file.read()
+
+        # Generating Soup
+        # Soup means the object representing 
+        # the DOM in your python code!
         soup = BS4(contents, 'html.parser')
         body = soup.body
 
@@ -31,7 +45,8 @@ for html in htmls:
         for s in body.select('script'):
             s.extract()
 
-        # Clearing 
+        # Clearing by querying using css classes and ids
+        # You need to look into your html files for these.
 
         ## Preloader 
         body.select('.preloader')[0].extract()       
@@ -47,7 +62,6 @@ for html in htmls:
             pass
 
 
-
         ## Footer
         body.select('.footer-area')[0].extract() 
         try:
@@ -61,21 +75,28 @@ for html in htmls:
         ###
         
         # Converting Soup ==> PlainText
+        # So that Regular Expression can be applied
         body_txt = str(body)
 
-        # Converting class to className
+        # Converting class to className for JSX support
         body_txt = re.sub(r'class=\"','className=\"', body_txt)
 
-        # Converting Comments
+        # Converting Comments for JSX support
+        #### You don't need this for Vue component conversion
         # body_txt = re.sub(r"<!--", "{/*", body_txt)
         # body_txt = re.sub(r"-->", "*/}", body_txt)
 
-        # Removing body Tag
+        # Removing excess body Tag
         body_txt = re.sub(r"</?body>","", body_txt)
         
-        # Creating React File
+        ### Creating Component File -----------------------
+
+        ################## Tweak here for React or Vue ##################
+        # Change the files extension --------------------------------------_V_
         component_file = os.path.join(component_folder, component_name + ".vue")
         with open(component_file,'w', encoding='utf-8') as f:
-            # f.write(rfc.format(a=component_name,b=body_txt))
-            f.write(sfc.format(b=body_txt))
+
+            # Switch 'comment out' between these two lines
+            # f.write(rfc.format(a=component_name,b=body_txt)) # For React
+            f.write(sfc.format(b=body_txt)) # For Vue
 
